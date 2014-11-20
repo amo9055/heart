e0c8d6b0838a82fabe2f8ca876e468a5fec69af0
Allison O'Connor <aoconnor@gmu.edu>
2014-11-19 22:25:10 -0500

    Initial Commit

---
+++ houses.R
@@ -0,0 +1,24 @@
+load("../FFXData/FFXDAta.RData")
+library(dplyr)
+library(ggplot2)
+
+# FFX_res_data %>%
+# select(decade, fireplaces) %>%
+
+#this function determines if a house has a fireplace or not.
+has_fireplace <- function(n) {
+ ifelse(n == 0, FALSE, TRUE)
+}
+
+
+FFX_res_data <- 
+  
+  FFX_res_data %>%
+#this adds a column which indicates if a fireplace is present (TRUE) or not (FALSE)
+  mutate(has_fp = has_fireplace(fireplaces)) %>%
+#this groups the data by decade and whether it has a fireplace
+  group_by(decade, has_fp) %>%
+#this counts the data
+  summarize(n = n()) %>%
+  ggplot(aes(x = decade, y = n, fill = has_fp)) + geom_bar(stat="identity", position="dodge")
+
