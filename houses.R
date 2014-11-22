4f171abf87100950fd531ddcc40d60c46603656e
Allison O'Connor <aoconnor@gmu.edu>
2014-11-22 08:20:30 -0500

    fixed wonky data issue

--- houses.R
+++ houses.R
@@ -7,13 +7,16 @@ library(ggplot2)
 has_fireplace <- function(n) {
   ifelse(n == 0, FALSE, TRUE)
 }
-FFX_res_data <-
-  FFX_res_data %>%
+FFX_res_data_sum <- FFX_res_data %>%
+  
   #this adds a column which indicates if a fireplace is present (TRUE) or not (FALSE)
   mutate(has_fp = has_fireplace(fireplaces)) %>%
+  
   #this groups the data by decade and whether it has a fireplace
   group_by(decade, has_fp) %>%
+  
   #this counts the data
-  summarize(n = n()) %>%
-  #this creates a bar graph from the data
-  ggplot(aes(x = decade, y = n, fill = has_fp)) + geom_bar(stat="identity", position="dodge")
+  summarize(n = n())
+
+
+
