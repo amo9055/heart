41d342dd680562b74ad496fc79afba02b8b2a672
Allison O'Connor <aoconnor@gmu.edu>
2014-11-26 09:40:42 -0500

    joined tables for calculation; cleaned up variable names

--- fp_per_house.R
+++ fp_per_house.R
@@ -2,9 +2,21 @@ load("../FFXData/FFXDAta.RData")
 library(dplyr)
 
 #calculate how many houses have x number of fireplaces
-
 FFX_number_fp_per_house <-FFX_res_data %>%
   select(fireplaces, decade) %>%
   group_by(decade, fireplaces) %>%
-  summarize(number_houses_w_x_fps = n())
+  summarize(houses_with_x_fps = n())
 
+#calculate how many houses were built per decade
+FFX_houses_built_per_decade <- FFX_res_data %>%
+  group_by(decade) %>%
+  summarize(houses_built = n())
+
+#join two tables together
+FFX_number_fp_per_house %>%
+  left_join(FFX_houses_built_per_decade, by = "decade")
+
+
+
+  
+ 
